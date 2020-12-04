7c0a791d035f30914897fe68bebf615b0f2b6125
Kavya-Das <kavyasree.das@gmail.com>
2020-12-04 12:26:53 +0530

    added script

--- index.php
+++ index.php
@@ -5,35 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
-    <h1>Library</h1>
+<body class="backcolor">
+    <h1 class="textcolor">Library</h1>
     <table class="table">
         <tr>
-            <td>Book Name:</td>
-            <td><input type="text" class="form-control"></td>
+            <td class="textcolor">Book Name:</td>
+            <td><input type="text" id="name"  class="form-control"></td>
         </tr>
         <tr>
-            <td>Author:</td>
-            <td><input type="text" class="form-control"></td>
+            <td class="textcolor">Author:</td>
+            <td><input type="text" id="author" class="form-control"></td>
         </tr>
         <tr>
-            <td>Distributor:</td>
-            <td><input type="text" class="form-control"></td>
+            <td class="textcolor">Distributor:</td>
+            <td><input type="text" id="distributor" class="form-control"></td>
         </tr>
         <tr>
-            <td>Category:</td>
-            <td><input type="radio" class="form-control" name="Category">History</td>
-            <td><input type="radio" class="form-control" name="Category">Stories</td>
+            <td class="textcolor">Category:</td>
+            <td><input type="radio" id="category" class="textcolor" name="Category">History<input type="radio" id="category" class="textcolor" name="Category">Stories</td>
         </tr>
         <tr>
-            <td>Price:</td>
-            <td><input type="text" class="form-control"></td>
+            <td class="textcolor">Price:</td>
+            <td><input type="text" id="price" class="form-control"></td>
         </tr>
         <tr>
             <td></td>
-            <td><button class="btn-info">Search</button></td>
+            <td><button onclick="clickme()" class="btn btn-info">Search</button></td>
         </tr>
     </table>
+    <script src="js\myscript.js">
+
+    </script>
 </body>
 </html>
---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+function clickme(){
+    var getname=document.getElementById("name").value
+    var getauthor=document.getElementById("author").value
+    var getdistributor=document.getElementById("distributor").value
+    var getcategory=document.getElementById("category").value
+    var getprice=document.getElementById("price").value
+    console.log(getname)
+    console.log(getauthor)
+    console.log(getdistributor)
+    console.log(getcategory)
+    console.log(getprice)
+}
